47d1a56b9779da3de1c30375825fae49e2cc192b
Dare7Dare <72170957+Dare7Dare@users.noreply.github.com>
2021-03-17 21:41:37 +0100

    no hamburger menu

--- js/main.js
+++ js/main.js
@@ -102,13 +102,13 @@ function getDateTime() {
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("button1");
+var btn1 = document.getElementById("button1");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn1.onclick = function() {
   modal.style.display = "block";
 }
 
@@ -117,11 +117,60 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+// Get the modal
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("button12");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on the button, open the modal
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
   }
 }
+
+$(function(){
+    $('.readmore a.more').on('click', function(){
+        var $parent = $(this).parent();
+        if($parent.data('visible')) {
+            $parent.data('visible', false).find('.ellipsis').show()
+            .end().find('.moreText').hide()
+            .end().find('a.more').text('show more');
+        } else {
+            $parent.data('visible', true).find('.ellipsis').hide()
+            .end().find('.moreText').show()
+            .end().find('a.more').text('show less');
+        }
+    });
+});
+
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
 
 });
